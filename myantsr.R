f7a800edd400813e8414180147e28bb7d48a73af
stnava <stnava@gmail.com>
2017-08-21 06:13:48 -0400

    WIP: test a different approach

--- myantsr.R
+++ myantsr.R
@@ -2,6 +2,7 @@ print("Begin")
 install.packages(
 c(
 "Rcpp","magrittr","dplyr","devtools",
+"pander",
 "pixmap",
 "png",
 "rmarkdown",
@@ -9,43 +10,45 @@ c(
 "testthat",
 "viridis" )
 )
-install.packages(
-c(
-"abind",
-"caret",
-"cluster",
-"d3Network",
-"DMwR",
-"e1071",
-"extremevalues",
-"fastICA",
-"fpc",
-"ggplot2",
-"glasso",
-"glmnet",
-"grid",
-"irlba",
-"knitr",
-"lme4",
-"magic",
-"MASS",
-"mFilter",
-"misc3d",
-"moments",
-"nabor",
-"pander",
-"pracma",
-"prettydoc",
-"psych",
-"randomForest",
-"rflann",
-"rgl",
-"robust",
-"robustbase",
-"signal",
-"sna",
-"visreg",
-"wmtsa")
-)
-devtools::install_github("igraph/rigraph")
-devtools::install_github( "stnava/ANTsR")
+library( devtools )
+if ( FALSE )
+  install.packages(
+  c(
+  "abind",
+  "caret",
+  "cluster",
+  "d3Network",
+  "DMwR",
+  "e1071",
+  "extremevalues",
+  "fastICA",
+  "fpc",
+  "ggplot2",
+  "glasso",
+  "glmnet",
+  "grid",
+  "irlba",
+  "knitr",
+  "lme4",
+  "magic",
+  "MASS",
+  "mFilter",
+  "misc3d",
+  "moments",
+  "nabor",
+  "pander",
+  "pracma",
+  "prettydoc",
+  "psych",
+  "randomForest",
+  "rflann",
+  "rgl",
+  "robust",
+  "robustbase",
+  "signal",
+  "sna",
+  "visreg",
+  "wmtsa")
+  )
+# devtools::install_github("igraph/rigraph")
+# devtools::install_github( "stnava/ANTsR")
